3b0c4d80773af6e57f62bb8d1fcf8cd4b58f3cb7
Mike Taylor <mike@indexdata.com>
2019-02-05 11:55:20 +0000

    Add a selection of v0.1 and v1.0 OpenURls

--- src/test/detectV01OpenUrl.js
+++ src/test/detectV01OpenUrl.js
@@ -1,11 +1,26 @@
-const { expect } = require('chai');
+const { assert, expect } = require('chai');
 const { isVersion0point1 } = require('../ContextObject');
+
+const v01urls = [
+  {},
+  { id: '1' },
+  { author: 'smith', title: 'water' },
+];
+
+const v10urls = [
+  { rft_id: '1' },
+  { 'rft.author': 'smith', 'rft.title': 'water' },
+];
 
 describe('detect v0.1 OpenURL', () => {
   it('does recognise version 0.1 OpenURLs', () => {
-    expect(isVersion0point1({})).to.be.true;
+    v01urls.forEach(val => {
+      assert.equal(isVersion0point1(val), true, `${JSON.stringify(val)} is v0.1`);
+    });
   });
   it('does not recognise version 1.0 OpenURLs', () => {
-    expect(isVersion0point1({ rft_id: '1' })).to.be.false;
+    v10urls.forEach(val => {
+      assert.equal(isVersion0point1(val), false, `${JSON.stringify(val)} is not v0.1`);
+    });
   });
 });
