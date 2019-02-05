7021cbcb0f19a071cf799b3f7c0cf8609803527c
Mike Taylor <mike@indexdata.com>
2019-02-05 16:38:30 +0000

    Parse query strings to yield OpenURLs to test

--- src/test/detectV01OpenUrl.js
+++ src/test/detectV01OpenUrl.js
@@ -1,27 +1,30 @@
 const { describe, it } = require('mocha');
 const { assert } = require('chai');
 const { isVersion0point1 } = require('../ContextObject');
+const querystring = require('querystring');
 
 const v01urls = [
-  {},
-  { id: '1' },
-  { author: 'smith', title: 'water' },
+  '',
+  'id=1',
+  'author=smith&title=water',
 ];
 
 const v10urls = [
-  { rft_id: '1' },
-  { 'rft.author': 'smith', 'rft.title': 'water' },
+  'rft_id=1',
+  'rft.author=smith&rft.title=water',
 ];
 
 describe('detect v0.1 OpenURL', () => {
   it('does recognise version 0.1 OpenURLs', () => {
     v01urls.forEach(val => {
-      assert.equal(isVersion0point1(val), true, `${JSON.stringify(val)} is v0.1`);
+      const parsed = querystring.parse(val);
+      assert.equal(isVersion0point1(parsed), true, `${val} is v0.1`);
     });
   });
   it('does not recognise version 1.0 OpenURLs', () => {
     v10urls.forEach(val => {
-      assert.equal(isVersion0point1(val), false, `${JSON.stringify(val)} is not v0.1`);
+      const parsed = querystring.parse(val);
+      assert.equal(isVersion0point1(parsed), false, `${val} is not v0.1`);
     });
   });
 });
