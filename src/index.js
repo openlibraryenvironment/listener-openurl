72a8fd49f95ebf8f243e17f572b44d54e0df750a
Mike Taylor <mike@indexdata.com>
2019-02-05 10:02:28 +0000

    First trivial version, logs incoming OpenURLs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('Hello, world!');
+const Koa = require('koa');
+const app = new Koa();
+// const Logger = require('stripes-logger');
+// const l = new Logger(process.env.LOGGING_CATEGORIES || process.env.LOGCAT);
+const ContextObject = require('./ContextObject');
+
+app.use(ctx => {
+  const co = new ContextObject(ctx.query);
+  console.log('got ContextObject', JSON.stringify(co, null, 2));
+  ctx.body = 'OpenURL request received\n';
+});
+
+app.listen(3000);
+console.log('ready');
