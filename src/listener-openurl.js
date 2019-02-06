1a2b4835aa1020b462a7230b96058027647d49ec
Mike Taylor <mike@indexdata.com>
2019-02-06 15:03:34 +0000

    Use categorical-logger

--- src/listener-openurl.js
+++ src/listener-openurl.js
@@ -1,14 +1,14 @@
 const Koa = require('koa');
 const app = new Koa();
-// const Logger = require('stripes-logger');
-// const l = new Logger(process.env.LOGGING_CATEGORIES || process.env.LOGCAT);
+const Logger = require('categorical-logger');
+const l = new Logger(process.env.LOGGING_CATEGORIES || process.env.LOGCAT);
 const { ContextObject } = require('./ContextObject');
 
 app.use(ctx => {
   const co = new ContextObject(ctx.query);
-  console.log(`got ContextObject ${co.getType()} query`, JSON.stringify(co.getQuery(), null, 2));
+  l.log('co', `got ContextObject ${co.getType()} query`, JSON.stringify(co.getQuery(), null, 2));
   ctx.body = 'OpenURL request received\n';
 });
 
 app.listen(3000);
-console.log('ready');
+l.log('start', 'starting');
