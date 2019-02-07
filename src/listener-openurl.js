9a036b83391e33f32e696d85166fa3b9379dac9f
Mike Taylor <mike@indexdata.com>
2019-02-07 17:51:30 +0000

    Make Okapi session and post ReShare requests to it

--- src/listener-openurl.js
+++ src/listener-openurl.js
@@ -2,8 +2,10 @@ const Koa = require('koa');
 const { Config } = require('./Config');
 const { ContextObject } = require('./ContextObject');
 const { ReshareRequest } = require('./ReshareRequest');
+const { OkapiSession } = require('./OkapiSession');
 
 const cfg = new Config();
+const okapi = new OkapiSession(cfg);
 const app = new Koa();
 
 app.use(ctx => {
@@ -11,9 +13,9 @@ app.use(ctx => {
   cfg.log('co', `got ContextObject ${co.getType()} query`, JSON.stringify(co.getQuery(), null, 2));
   cfg.log('admindata', JSON.stringify(co.getAdmindata(), null, 2));
   cfg.log('metadata', JSON.stringify(co.getMetadata(), null, 2));
-  const reshareRequest = new ReshareRequest(co);
-  cfg.log('rr', JSON.stringify(reshareRequest.getRequest(), null, 2));
-  // POST the request to mod-rs
+  const rr = new ReshareRequest(co);
+  cfg.log('rr', JSON.stringify(rr.getRequest(), null, 2));
+  okapi.post('/rs/patronrequests', rr);
   ctx.body = 'OpenURL request received\n';
 });
 
