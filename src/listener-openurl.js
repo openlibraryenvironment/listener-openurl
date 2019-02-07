c80db0bb806cde7fe70f742c3242d2a01829c2a9
Mike Taylor <mike@indexdata.com>
2019-02-07 15:25:41 +0000

    Make config, use its delegated log function

--- src/listener-openurl.js
+++ src/listener-openurl.js
@@ -1,21 +1,22 @@
-const Logger = require('categorical-logger');
 const Koa = require('koa');
+const { Config } = require('./Config');
 const { ContextObject } = require('./ContextObject');
 const { ReshareRequest } = require('./ReshareRequest');
 
-const l = new Logger(process.env.LOGGING_CATEGORIES || process.env.LOGCAT);
+const cfg = new Config();
+console.log('cfg =', cfg);
 const app = new Koa();
 
 app.use(ctx => {
   const co = new ContextObject(ctx.query);
-  l.log('co', `got ContextObject ${co.getType()} query`, JSON.stringify(co.getQuery(), null, 2));
-  l.log('admindata', JSON.stringify(co.getAdmindata(), null, 2));
-  l.log('metadata', JSON.stringify(co.getMetadata(), null, 2));
+  cfg.log('co', `got ContextObject ${co.getType()} query`, JSON.stringify(co.getQuery(), null, 2));
+  cfg.log('admindata', JSON.stringify(co.getAdmindata(), null, 2));
+  cfg.log('metadata', JSON.stringify(co.getMetadata(), null, 2));
   const reshareRequest = new ReshareRequest(co);
-  l.log('rr', JSON.stringify(reshareRequest.getRequest(), null, 2));
+  cfg.log('rr', JSON.stringify(reshareRequest.getRequest(), null, 2));
   // POST the request to mod-rs
   ctx.body = 'OpenURL request received\n';
 });
 
 app.listen(3000);
-l.log('start', 'starting');
+cfg.log('start', 'starting');
