1ada93201cf2330bb6ebea93dc2c7eb9c77b4996
Mike Taylor <mike@indexdata.com>
2019-02-07 12:21:09 +0000

    Create and log a ReshareRequest

--- src/listener-openurl.js
+++ src/listener-openurl.js
@@ -1,6 +1,7 @@
 const Logger = require('categorical-logger');
 const Koa = require('koa');
 const { ContextObject } = require('./ContextObject');
+const { ReshareRequest } = require('./ReshareRequest');
 
 const l = new Logger(process.env.LOGGING_CATEGORIES || process.env.LOGCAT);
 const app = new Koa();
@@ -10,6 +11,9 @@ app.use(ctx => {
   l.log('co', `got ContextObject ${co.getType()} query`, JSON.stringify(co.getQuery(), null, 2));
   l.log('admindata', JSON.stringify(co.getAdmindata(), null, 2));
   l.log('metadata', JSON.stringify(co.getMetadata(), null, 2));
+  const reshareRequest = new ReshareRequest(co);
+  l.log('rr', JSON.stringify(reshareRequest.getRequest(), null, 2));
+  // POST the request to mod-rs
   ctx.body = 'OpenURL request received\n';
 });
 
