4c202f7d7264af5366ee5d45e989d0f26b4c98ae
Mike Taylor <mike@indexdata.com>
2019-02-08 17:04:55 +0000

    Use server from new OpenURLServer.js

--- src/listener-openurl.js
+++ src/listener-openurl.js
@@ -1,23 +1,8 @@
-const Koa = require('koa');
 const { Config } = require('./Config');
-const { ContextObject } = require('./ContextObject');
-const { ReshareRequest } = require('./ReshareRequest');
-const { OkapiSession } = require('./OkapiSession');
+const { OpenURLServer } = require('./OpenURLServer');
 
 const cfg = new Config();
-const okapi = new OkapiSession(cfg);
-const app = new Koa();
+const server = new OpenURLServer(cfg);
 
-app.use(ctx => {
-  const co = new ContextObject(cfg, ctx.query);
-  cfg.log('co', `got ContextObject ${co.getType()} query`, JSON.stringify(co.getQuery(), null, 2));
-  cfg.log('admindata', JSON.stringify(co.getAdmindata(), null, 2));
-  cfg.log('metadata', JSON.stringify(co.getMetadata(), null, 2));
-  const rr = new ReshareRequest(co);
-  cfg.log('rr', JSON.stringify(rr.getRequest(), null, 2));
-  okapi.post('/rs/patronrequests', rr);
-  ctx.body = 'OpenURL request received\n';
-});
-
-app.listen(3000);
+server.getApp().listen(3000);
 cfg.log('start', 'starting');
