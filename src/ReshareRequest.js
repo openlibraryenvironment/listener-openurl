23e099bb4168b0876efcc48dfef73007049de103
Mike Taylor <mike@indexdata.com>
2019-02-20 13:20:05 +0000

    Set more fields in the ReShare request
    Fixes PR-125

--- src/ReshareRequest.js
+++ src/ReshareRequest.js
@@ -10,6 +10,32 @@ const _ = require('lodash');
 const uuidv4 = require('uuid/v4');
 
 
+// From OpenURL v0.1 specifcation, table in section 7, valid genres are:
+//      journal, book, conference, article, preprint, proceeding, bookitem
+//
+// The OpenURL v1.0 specification is characteristically coy about
+// genre, suggesting only that it's arbitrarily extensible and
+// accidentally giving away a list of acceptable values in the XML
+// Metadata Format for "journal" (Table 19). This is mostly the set of
+// v0.1 genres, but with "book" and "bookitem" omitted (reasonably
+// enough since this specific schema is only about journals) but with
+// the addition of "issue" and "unknown".
+//
+// Since I am not so far able to determine what the set of possible
+// values is in mod-rs, we just pass values through for now.
+//
+function genreToPublicatonType(genre) {
+  return genre;
+}
+
+
+// Complete set of OpenURL v0.1 fields, drawn from table on pages 6-7 of the specification:
+//      genre, aulast, aufirst, auinit, auinit1, auinitm, issn, eissn,
+//      coden, isbn, sici, bici, title, stitle, atitle, volume, part,
+//      issue, spage, epage, pages, artnum, date, ssn, quarter
+//
+// (There is no analogous list for OpenURL v1.0)
+//
 function translateCOtoRR(co) {
   const a = co.getAdmindata();
   const m = co.getMetadata();
@@ -17,34 +43,47 @@ function translateCOtoRR(co) {
 
   // I assume this is the ID of the request itself
   rr.id = _.get(a, 'ctx.id') || uuidv4();
-  // XXX publicationType
+  rr.publicationType = genreToPublicatonType(_.get(m, 'rft.genre'));
 
   // This all seems to be metadata about the requested item
   rr.title = _.get(m, 'rft.title') || _.get(m, 'rft.btitle') || _.get(m, 'rft.atitle') || _.get(m, 'rft.jtitle');
   rr.author = _.get(m, 'rft.au') || _.get(m, 'rft.aulast') || _.get(m, 'rft.aulast');
-  // XXX subtitle
-  // XXX sponsoringBody
-  // XXX publisher
-  // XXX placeOfPublication
-  // XXX volume
-  // XXX issue
-  // XXX startPage
-  // XXX numberOfPages
-  // XXX publicationDate
-  // XXX publicationDateOfComponent
-  // XXX edition
+  // rr.subtitle has no corresponding OpenURL field
+  // rr.sponsoringBody has no corresponding OpenURL field
+  // rr.publisher has no corresponding OpenURL field
+  // rr.placeOfPublication has no corresponding OpenURL field
+  rr.volume = _.get(m, 'rft.volume');
+  rr.issue = _.get(m, 'rft.issue');
+  rr.startPage = _.get(m, 'rft.spage');
+  const spage = _.get(m, 'rft.spage');
+  const epage = _.get(m, 'rft.epage');
+  const pages = _.get(m, 'rft.pages');
+  if (spage && epage) {
+    rr.numberOfPages = epage - spage + 1;
+  } else if (pages) {
+    const match = pages.match(/^(\d+)-(\d+)$/);
+    if (match) {
+      rr.numberOfPages = match[2] - match[1] + 1;
+    }
+  }
+  rr.publicationDate = _.get(m, 'rft.date');
+  // rr.publicationDateOfComponent has no corresponding OpenURL field
+  // rr.edition has no corresponding OpenURL field
 
   // Administrative data about who is asking for what
   rr.patronReference = _.get(a, 'req.id');
   rr.serviceType = _.get(a, 'svc.id'); // No example of this in Z39.88
 
-  // XXX isRequester
-
-  // XXX No idea what any of these are until the schema gets bulked out
-  rr.state = undefined;
-  // XXX numberOfRetries
-  // XXX delayPerformingActionUntil
-  // XXX pendingAction
+  // All of the following are probably used only internally
+  // rr.isRequester
+  // rr.state;
+  // rr.numberOfRetries
+  // rr.delayPerformingActionUntil
+  // rr.pendingAction
+  // rr.errorAction;
+  // rr.preErrorStatus;
+  // rr.awaitingProtocolResponse;
+  // rr.rotaPosition;
 
   rr.tags = undefined;
   rr.customProperties = undefined;
