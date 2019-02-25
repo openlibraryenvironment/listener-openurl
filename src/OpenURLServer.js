2385474d47fbf5d2f6c13b91c70f7546ad677e4c
Mike Taylor <mike@indexdata.com>
2019-02-25 18:32:44 +0000

    Completely rewrite static server to use koa-static
    *sigh*
    
    It hurt to throw away code, but this is the right thing.

--- src/OpenURLServer.js
+++ src/OpenURLServer.js
@@ -1,6 +1,7 @@
 // Creates an OpenURL server that contains a configured Koa app.
 
 const Koa = require('koa');
+const KoaStatic = require('koa-static')
 const _ = require('lodash');
 const { ContextObject } = require('./ContextObject');
 const { ReshareRequest } = require('./ReshareRequest');
@@ -12,12 +13,16 @@ class OpenURLServer {
     this.okapi = new OkapiSession(cfg);
     this.cfg = cfg;
 
+    const docRoot = this.cfg.getValues().docRoot;
+    if (!docRoot) {
+      throw new HTTPError(500, 'no docRoot defined in configuration');
+    }
+
     this.app = new Koa();
-    this.app.use(ctx => {
-      if (ctx.path === '/favicon.ico') {
-        return this.serveStatic(ctx, 'favicon.ico');
-      } else if (ctx.path.startsWith('/static/')) {
-        return this.serveStatic(ctx, ctx.path.substring(8));
+    this.app.use(async (ctx, next) => {
+      if (ctx.path.startsWith('/static/') || ctx.path === '/favicon.ico') {
+        await next();
+        return;
       }
 
       const co = new ContextObject(cfg, ctx.query);
@@ -54,41 +59,11 @@ class OpenURLServer {
             });
         });
     });
+    this.app.use(KoaStatic(`${cfg.path}/${docRoot}`));
   }
 
   okapiLogin() { return this.okapi.login(); }
   listen(...args) { return this.app.listen(...args); }
-
-  serveStatic(ctx, filename) {
-    if (filename.indexOf('../') === 0 || filename.indexOf('/../') !== -1) {
-      throw new HTTPError(403, 'unsanitary path');
-    }
-
-    const dir = this.cfg.getValues().staticPath;
-    if (!dir) {
-      throw new HTTPError(500, 'no staticPath defined in configuration');
-    }
-
-    try {
-      ctx.body = this.cfg.readFile(`${dir}/${filename}`);
-    } catch (e) {
-      if (e.code === 'ENOENT') throw new HTTPError(404, `cannot find '${filename}'`);
-      if (e.code === 'EACCES') throw new HTTPError(403, `permission denied for '${filename}'`);
-      throw new HTTPError(500, `system error reading '${filename}'`);
-    }
-
-    // Perhaps this table should be read from the configuration? Maybe not.
-    const ext2contentType = {
-      'txt': 'text/plain',
-      'html': 'text/html',
-      'png': 'image/png',
-      'gif': 'image/gif',
-      'jpeg': 'image/jpeg',
-      'jpg': 'image/jpeg',
-    };
-    const ext = filename.replace(/.*\./, '').toLowerCase();
-    ctx.type = ext2contentType[ext];
-  }
 
   htmlBody(res, text) {
     const status = `${res.status}`;
