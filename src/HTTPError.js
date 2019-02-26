b968b6d375fa1bfa198399186d0f45c9feb97da9
Mike Taylor <mike@indexdata.com>
2019-02-26 10:26:24 +0000

    Fix case of classname

--- src/HTTPError.js
+++ src/HTTPError.js
@@ -1,4 +1,4 @@
-class HttpError extends Error {
+class HTTPError extends Error {
   constructor(response, comment) {
     let status, url, maybeFor;
     if (typeof response === 'number') {
@@ -12,11 +12,11 @@ class HttpError extends Error {
     maybeFor = url ? ` for ${url}` : '';
     super(`${status}${maybeFor}: ${comment}`);
     this.response = response;
-    this.name = 'HttpError';
+    this.name = 'HTTPError';
     this.status = status;
     this.expose = true; // Not true by default for 500 errors
     this.comment = comment;
   }
 }
 
-module.exports = HttpError;
+module.exports = HTTPError;
