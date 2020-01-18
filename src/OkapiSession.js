fd3318b8170934a5debf5db825617a0c7d7976d6
Mike Taylor <mike@indexdata.com>
2020-01-18 11:36:37 -0500

    Simplify, prepare for muliple OkapiSession objects

--- src/OkapiSession.js
+++ src/OkapiSession.js
@@ -5,19 +5,22 @@ const HTTPError = require('./HTTPError');
 
 class OkapiSession {
   constructor(cfg) {
-    this.cfg = cfg;
-    cfg.log('okapi', 'making Okapi session');
-    const values = cfg.getValues();
+    this.logger = cfg;
+
     const filename = cfg.getFilename();
-    this.okapiUrl = values.okapiUrl;
-    this.tenant = values.tenant;
-    if (!this.okapiUrl) throw Error(`no okapiUrl defined in ${filename}`);
-    if (!this.tenant) throw Error(`no tenant defined in ${filename}`);
+    const values = cfg.getValues();
+    ['okapiUrl', 'tenant', 'username', 'password'].forEach(param => {
+      const val = values[param];
+      if (!val) throw Error(`no ${param} defined in ${filename}`);
+      this[param] = val;
+    });
+
+    cfg.log('okapi', `making Okapi session for ${this.okapiUrl}`);
   }
 
   login() {
-    const { username, password } = this.cfg.getValues();
-    this.cfg.log('okapi', `logging into Okapi as ${username}/${password}`);
+    const { username, password } = this;
+    this.logger.log('okapi', `logging into Okapi as ${username}/${password}`);
     return this.post('/authn/login', { username, password })
       .then(res => {
         if (res.status !== 201) throw new HTTPError(res, 'cannot login to FOLIO');
@@ -32,7 +35,7 @@ class OkapiSession {
       'x-okapi-tenant': this.tenant,
     };
     if (this.token) headers['x-token-token'] = this.token;
-    this.cfg.log('okapi', `POST to ${path}`);
+    this.logger.log('okapi', `POST to ${path}`);
     return fetch(`${this.okapiUrl}${path}`, {
       method: 'POST',
       body: JSON.stringify(payload),
