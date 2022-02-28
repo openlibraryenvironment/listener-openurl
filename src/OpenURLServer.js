c7be1313465d93e632da8487977c39dd63abe581
Ian Hardy <ian.c.hardy@gmail.com>
2022-02-28 16:07:08 -0500

    If there is only one pickup location, set it as the selected value on the form

--- src/OpenURLServer.js
+++ src/OpenURLServer.js
@@ -200,6 +200,7 @@ class OpenURLServer {
     const data = Object.assign({}, query, {
       allValues,
       noPickupLocation: ntries > 0 && !query['svc.pickupLocation'],
+      onePickupLocation: (service.pickupLocations.length == 1),
       pickupLocations: (service.pickupLocations || []).map(x => ({
         id: x.id,
         code: x.code,
